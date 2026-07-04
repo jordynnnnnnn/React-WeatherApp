b79b8f302feca6f7b3553474b592500d1ed0213a
jordynnnnnnn <jordyn.pilcher@hotmail.com>
2026-07-04 14:30:10 +0930

    Added footer with open sourced GitHub link

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+
+export default function Footer() {
+  return (
+    <footer>
+      <h4>
+        This app was coded by Jordyn Pilcher and is open-sourced on <a href="https://github.com/jordynnnnnnn/React-WeatherApp">GitHub</a>
+      </h4>
+    </footer>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
+import "./styles.css";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import Footer from "./Footer";
 
 const rootElement = document.getElementById("root");
 const root = ReactDOM.createRoot(rootElement);
@@ -8,5 +10,6 @@ const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
     <App />
+    <Footer />
   </React.StrictMode>,
 );
